82d8447ab2a059a23d6c84e67d44dd29323a73b8
coolcombo3 <73761067+coolcombo3@users.noreply.github.com>
2020-11-01 17:31:34 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./styles.css";
+
+const ferretPic = document.getElementById("ferretPic");
+const bigButton = document.getElementById("bigButton");
+const smallButton = document.getElementById("smallButton");
+const moveRightButton = document.getElementById("moveRight");
+const moveLeftButton = document.getElementById("moveLeft");
+const resetButton = document.getElementById("reset");
+
+const getDims = () => ferretPic.getBoundingClientRect();
+
+let picInfo = getDims();
+
+bigButton.addEventListener("click", function () {
+  ferretPic.height = ferretPic.height + 10;
+});
+
+smallButton.addEventListener("click", function () {
+  ferretPic.height = ferretPic.height - 10;
+});
+
+moveRightButton.addEventListener("click", function () {
+  picInfo.x = picInfo.x + 10;
+  ferretPic.style = `margin-left: ${picInfo.x}px`;
+});
+
+moveLeftButton.addEventListener("click", function () {
+  picInfo.x = picInfo.x - 10;
+  ferretPic.style = `margin-left: ${picInfo.x}px`;
+});
+
+resetButton.addEventListener("click", () => {
+  ferretPic.style = "margin-left: 0";
+  ferretPic.height = 200;
+  picInfo = getDims();
+});
